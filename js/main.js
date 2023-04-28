5bcfc749587c44d6df2bdaf5b8e017b63dbbe3d6
Sage316 <115668242+Sage316@users.noreply.github.com>
2023-04-28 18:33:05 +0200

    added the sticky navbar and most of the css

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const product = [
     {
         id: 0,
         image: 'image/md-salman-tWOz2_EK5EQ-unsplash.jpg',
-        title: 'T-shits',
+        title: 'T-shirts',
         price: 120,
     },
     {
@@ -22,6 +22,30 @@ const product = [
         image: 'image/johnstons-of-elgin-dMqbaI2IbHs-unsplash.jpg',
         title: 'Jersey',
         price: 170,
+    },
+    {
+        id: 0,
+        image: 'image/ali-muhamad-hT1R6Z5pY5I-unsplash.jpg',
+        title: 'Shirts',
+        price: 150,
+    },
+    {
+        id: 1,
+        image: 'image/nikhil-8Al2ylhlCYk-unsplash.jpg',
+        title: 'Socks',
+        price: 60,
+    },
+    {
+        id: 2,
+        image: 'image/brian-hall-x5aavOm7PFc-unsplash.jpg',
+        title: 'Shoes',
+        price: 1250,
+    },
+    {
+        id: 3,
+        image: 'image/kelly-sikkema-CNjfgzoY8JU-unsplash.jpg',
+        title: 'scarves',
+        price: 100,
     }
 ];
 
@@ -36,10 +60,10 @@ document.getElementById('root').innerHTML = categories.map((item) => {
     var { image, title, price } = item;
     return (
         `<div class='box'>
-            <div class='img-box'>
-                <img class='images' src=${image}></img>
-            </div>
+   
         <div class='bottom'>
+        <img class='images' src=${image}></img>
+
         <p>${title}</p>
         <h2>R ${price}.00</h2>` +
         "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
@@ -49,4 +73,18 @@ document.getElementById('root').innerHTML = categories.map((item) => {
 }).join('')
 
 var cart = [];
+
+// sticky navbar funtion
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("header");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
