8964c2cb6649a4bdd4a7f1a297750607c5546794
Sage316 <115668242+Sage316@users.noreply.github.com>
2023-04-28 11:13:01 +0200

    added correct images and fixing the add to cart funtion

--- js/main.js
+++ js/main.js
@@ -1,48 +1,52 @@
 const product = [
     {
         id: 0,
-        image: 'image/gg-1.jpg',
-        title: 'Z Flip Foldable Mobile',
+        image: 'image/md-salman-tWOz2_EK5EQ-unsplash.jpg',
+        title: 'T-shits',
         price: 120,
     },
     {
         id: 1,
-        image: 'image/hh-2.jpg',
-        title: 'Air Pods Pro',
-        price: 60,
+        image: 'image/tobias-tullius-Fg15LdqpWrs-unsplash.jpg',
+        title: 'Jackets',
+        price: 300,
     },
     {
         id: 2,
-        image: 'image/ee-3.jpg',
-        title: '250D DSLR Camera',
-        price: 230,
+        image: 'image/mnz-m1m2EZOZVwA-unsplash.jpg',
+        title: 'Jeans',
+        price: 250,
     },
     {
         id: 3,
-        image: 'image/aa-1.jpg',
-        title: 'Head Phones',
-        price: 100,
+        image: 'image/johnstons-of-elgin-dMqbaI2IbHs-unsplash.jpg',
+        title: 'Jersey',
+        price: 170,
     }
- ];
- const categories = [...new Set(product.map((item)=>
-    {return item}))]
-    let i=0;
- document.getElementById('root').innerHTML = categories.map((item)=>
- {
-    var {image, title, price} = item;
-    return(
+];
+
+const categories = [...new Set(product.map((item) => {
+    return item
+}
+)
+)
+]
+let i = 0;
+document.getElementById('root').innerHTML = categories.map((item) => {
+    var { image, title, price } = item;
+    return (
         `<div class='box'>
             <div class='img-box'>
                 <img class='images' src=${image}></img>
             </div>
         <div class='bottom'>
         <p>${title}</p>
-        <h2>R ${price}.00</h2>`+
-        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        <h2>R ${price}.00</h2>` +
+        "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
         `</div>
         </div>`
     )
- }).join('')
- 
- var cart =[];
- 
+}).join('')
+
+var cart = [];
+
